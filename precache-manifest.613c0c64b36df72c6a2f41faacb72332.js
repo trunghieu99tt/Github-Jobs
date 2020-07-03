98c6c9a88e4e9424a878bee79700c8012c5b691d
Rikikudo <trunghieu99tt@gmail.com>
2020-07-04 00:34:20 +0700

    Updates

---
+++ precache-manifest.613c0c64b36df72c6a2f41faacb72332.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97277721e19d1701422f734d62f9eb88",
+    "url": "/Github-Jobs/index.html"
+  },
+  {
+    "revision": "5d09ea37609219b815d6",
+    "url": "/Github-Jobs/static/css/main.21535552.chunk.css"
+  },
+  {
+    "revision": "d590652c0e24dfce0ddd",
+    "url": "/Github-Jobs/static/js/2.6cb77c58.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Github-Jobs/static/js/2.6cb77c58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d09ea37609219b815d6",
+    "url": "/Github-Jobs/static/js/main.c7769403.chunk.js"
+  },
+  {
+    "revision": "c4253f8433f1c38b33e9",
+    "url": "/Github-Jobs/static/js/runtime-main.156afc36.js"
+  },
+  {
+    "revision": "fc568113ad72433c2e3802588369dd2a",
+    "url": "/Github-Jobs/static/media/backgroundImg.fc568113.png"
+  }
+]);
